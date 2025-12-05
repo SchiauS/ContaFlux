e1c6afc8f89656bc38dffb9e07bfbaa5f5987c00
Schiau Sebastian <hermes.skilled@yahoo.com>
2025-12-05 14:39:52 +0200

    Update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,30 +26,61 @@
     </style>
 </head>
 <body class="g-sidenav-show bg-gray-100">
-    <nav class="navbar navbar-expand-lg navbar-main py-3">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="{{ route('dashboard') }}">ContaFlux</a>
-            <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
-                <span class="navbar-toggler-icon text-white"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('companies.index') }}">Companii</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('accounts.index') }}">Conturi</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('transactions.index') }}">Tranzacții</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tasks.index') }}">Task-uri</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<div class="min-height-300 bg-dark position-absolute w-100"></div>
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+    <div class="sidenav-header">
+        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+        <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
+            <img src="{{''}}" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo">
+            <span class="ms-1 font-weight-bold">ContaFlux</span>
+        </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="{{ route('dashboard') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-tv-2 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('companies.index') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-calendar-grid-58 text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Companii</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('accounts.index') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Conturi</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('transactions.index') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-app text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Tranzacții</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('tasks.index') }}">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="ni ni-app text-dark text-sm opacity-10"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Task-uri</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    </aside>
 
     <div class="container-fluid py-4">
         @yield('content')
