26688f949d7f220351dc90cbafdb8f13590d89ff
Schiau Sebastian <hermes.skilled@yahoo.com>
2025-12-05 21:09:06 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-building text-primary"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Companii</span>
+                    <span class="nav-link-text ms-1">Companie</span>
                 </a>
             </li>
             <li class="nav-item">
